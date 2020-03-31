b49dcb15204e7c451317a5688d53f9438d18521b
Andreas Albert <andreas.albert@cern.ch>
2020-03-31 12:40:28 -0400

    Remove fillers

--- bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/GJets_Gpt_1j_5f_NLO/fill_templates.sh
+++
@@ -1,9 +0,0 @@
-for DIR in ./*/; do
-    NAME=$(basename $DIR)
-    cp cuts_template.f $DIR/${NAME}_cuts.f
-done
-rm GJets_1j_Gpt-650ToInf_5f_NLO/*cuts.f
-
-sed -i "s/@PHOTONPTMAX/250/g" GJets_1j_Gpt-100To250_5f_NLO/*cuts.f
-sed -i "s/@PHOTONPTMAX/400/g" GJets_1j_Gpt-250To400_5f_NLO/*cuts.f
-sed -i "s/@PHOTONPTMAX/650/g" GJets_1j_Gpt-400To650_5f_NLO/*cuts.f
